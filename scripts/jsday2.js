03827d7de5fadae72145bb868d8b73cfb9bcaff1
rbbods <ronbrianbodomo@gmail.com>
2023-10-20 09:47:32 +0800

    First commit, hello world!

---
+++ scripts/jsday2.js
@@ -0,0 +1,36 @@
+// 2.
+var length = 0; width = 0; height = 0;
+// 3.
+width = 10;
+height = 15;
+length = 23;
+// 4.
+var product = length*width*height;
+console.log(product);
+// 5.
+if (product % 2 == 0) {
+    console.log("product is even number");
+} else {
+    console.log("product is even number");
+}
+
+if (product % 3 == 0) {
+    console.log("divisible by 3");
+} else {
+    console.log("not divisible by 3");
+}
+
+if (confirm('Press a button')){
+    console.log('You pressed OK');
+} else {
+    console.log('You pressed cancel');
+}
+
+console.log('end');
+
+var person = prompt('Please enter your name: ');
+if (person == 'Carlos') {
+    alert("Welcome to my site, " + person);
+} else {
+    alert('Only Alixandria can enter here!');
+}
